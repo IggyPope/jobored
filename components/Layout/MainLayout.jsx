f59fd084803b77562b9bd6575ac1a4b34895d4a4
IggyPope <altko4egar@gmail.com>
2023-05-07 23:08:49 +0300

    fix: transfer the font connection to MainLayout

--- components/Layout/MainLayout.jsx
+++ components/Layout/MainLayout.jsx
@@ -1,10 +1,17 @@
 import Header from '../Header/Header';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin', 'cyrillic'],
+  weight: ['400', '500', '600', '700'],
+  preload: true,
+});
 
 export default function MainLayout({ children }) {
   return (
-    <>
+    <div className={inter.className}>
       <Header />
       <main>{children}</main>
-    </>
+    </div>
   );
 }
