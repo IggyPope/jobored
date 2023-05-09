5467c7b24f34426076d1a6dbd8a4d6d224a2b4e6
IggyPope <altko4egar@gmail.com>
2023-05-09 18:29:15 +0300

    style: move route [slug] to folder name

--- pages/vacancies/[id].jsx
+++
@@ -1,8 +0,0 @@
-import { Title } from '@mantine/core';
-import { useRouter } from 'next/router';
-
-export default function Vacancies() {
-  const router = useRouter();
-
-  return <Title>Vacancies/{router.route}</Title>;
-}
---
+++ pages/vacancies/[id]/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from '@mantine/core';
+
+export default function Vacancies() {
+  return <Container size="md" px="lg" mt={40}></Container>;
+}
