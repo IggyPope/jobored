87257a7a47f29112e8491fe3a2541a9ad7062c91
IggyPope <altko4egar@gmail.com>
2023-05-09 22:42:12 +0300

    feat: create single vacancy page

--- pages/vacancies/[id]/index.jsx
+++ pages/vacancies/[id]/index.jsx
@@ -1,5 +1,15 @@
-import { Container } from '@mantine/core';
+import { Container, Flex, Paper } from '@mantine/core';
 
-export default function Vacancies() {
-  return <Container size="md" px="lg" mt={40}></Container>;
+export default function Vacancy() {
+  return (
+    <Container size="md" px="lg" mt={40}>
+      <Flex direction="row" align="flex-start" justify="stretch" gap="md">
+        <Paper radius="lg" p="xl" withBorder={true} sx={{ width: '100%' }}>
+          <p>Менеджер-дизайнер</p>
+          <p>з/п от 70000 rub * Полный рабочий день</p>
+          <p>Новый Уренгой</p>
+        </Paper>
+      </Flex>
+    </Container>
+  );
 }
