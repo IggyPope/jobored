2c8e7ff7f3a47adfabaa63d3fbf952697a399760
IggyPope <altko4egar@gmail.com>
2023-05-18 02:51:24 +0300

    chore: delete unnecessary files with client-side fetching

--- pages/api/hello.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  setTimeout(
-    () => res.status(200).json({ name: 'John Doe', id: req.query.id }),
-    5000
-  );
-}
