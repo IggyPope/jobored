489605d46f34743c0f82e003240e55eeeca6f5ce
IggyPope <altko4egar@gmail.com>
2023-05-17 18:49:04 +0300

    fix: added X-Api-App-Id token to all requests to avoid infinite redirects

--- pages/api/vacancies/getOneById.js
+++ pages/api/vacancies/getOneById.js
@@ -1,15 +1,11 @@
 import serverInstance from '@/services/api/serverInstance';
 
-const X_SECRET_KEY = process.env.API_X_SECRET_KEY;
-const CLIENT_SECRET = process.env.API_CLIENT_SECRET;
-
 export default async function handler(req, res) {
   serverInstance
     .get(`vacancies/${req.query.id}/`, {
       headers: {
         common: {
           Authorization: req.headers['authorization'],
-          'X-Api-App-Id': CLIENT_SECRET,
         },
       },
     })
