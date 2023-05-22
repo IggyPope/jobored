b0a964beda6928b73ba28d3099b11699fabbc0bf
IggyPope <altko4egar@gmail.com>
2023-05-22 18:56:42 +0300

    feat: implement favorites page

--- pages/favorites/index.jsx
+++ pages/favorites/index.jsx
@@ -1,33 +1,64 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Container } from '@mantine/core';
+import { Container, Pagination, Stack } from '@mantine/core';
 
 import { useFavorites } from '@/contexts/FavoritesContext';
 
 import VacancyCard from '@/components/Vacancy/VacancyCard/VacancyCard';
-import VacancyCardSkeleton from '@/components/Vacancy/VacancyCard/VacancyCardSkeleton';
+
+import { ITEMS_PER_PAGE } from '@/constants/constants';
+import NextIcon from '@/components/Icons/NextIcon';
+import NotFound from '@/components/NotFound/NotFound';
 
 export default function Favorites() {
   const { favorites } = useFavorites();
-  const [vacancies, setVacancies] = useState();
+  const [vacancies, setVacancies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     setVacancies(Object.values(favorites));
+    setIsLoading(false);
   }, [favorites]);
 
-  // const [vacancies, setVacancies] = useState({});
-  // const [page, setPage] = useState(1);
-
   return (
-    <Container size="md" px="lg" my={40}>
-      {vacancies?.length ? (
-        vacancies.map((vacancy) => (
-          <VacancyCard key={vacancy.id} vacancy={vacancy} />
-        ))
-      ) : (
-        <VacancyCardSkeleton />
-      )}
+    <Container size="md" px="lg" py={40}>
+      <Stack justify="space-between" spacing="xl">
+        <Stack spacing="sm">
+          {!isLoading && vacancies.length === 0 ? (
+            <NotFound />
+          ) : (
+            vacancies
+              .slice(
+                (page - 1) * ITEMS_PER_PAGE,
+                (page - 1) * ITEMS_PER_PAGE + ITEMS_PER_PAGE
+              )
+              .map((vacancy) => {
+                return <VacancyCard key={vacancy.id} vacancy={vacancy} />;
+              })
+          )}
+        </Stack>
+        {vacancies?.length > ITEMS_PER_PAGE && (
+          <Pagination
+            value={page}
+            onChange={setPage}
+            position="center"
+            bottom={10}
+            radius="xs"
+            spacing={8}
+            total={Math.ceil(vacancies?.length / ITEMS_PER_PAGE)}
+            styles={(theme) => ({
+              control: {
+                borderColor: theme.colors.gray[2],
+                fontSize: theme.fontSizes.sm,
+              },
+            })}
+            nextIcon={NextIcon}
+          />
+        )}
+      </Stack>
     </Container>
   );
 }
