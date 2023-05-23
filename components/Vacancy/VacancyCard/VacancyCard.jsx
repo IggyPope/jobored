674b7f8865dd15a9de6d04548049fdad742818fc
IggyPope <altko4egar@gmail.com>
2023-05-23 15:02:06 +0300

    feat: adaptive styling for VacancyCard

--- components/Vacancy/VacancyCard/VacancyCard.jsx
+++ components/Vacancy/VacancyCard/VacancyCard.jsx
@@ -7,19 +7,22 @@ import {
   Title,
   useMantineTheme,
 } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
+
 import FavoriteButton from '@/components/Buttons/FavoriteButton';
 import LocationIcon from '@/components/Icons/LocationIcon';
 import VacancyPayment from '../common/VacancyPayment';
 
 export default function VacancyCard({ vacancy }) {
   const theme = useMantineTheme();
+  const smallScreen = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
 
   return (
     <Link href={`/vacancies/${vacancy.id}`}>
       <Paper
         data-elem={`vacancy-${vacancy.id}`}
         radius="lg"
-        p="xl"
+        p={smallScreen ? 'sm' : 'xl'}
         w="100%"
         withBorder
         sx={(theme) => ({
@@ -31,7 +34,7 @@ export default function VacancyCard({ vacancy }) {
           },
         })}
       >
-        <Stack spacing="xs">
+        <Stack spacing={smallScreen ? 'xxs' : 'xs'}>
           <Flex direction="row" justify="space-between" align="center">
             <Title order={4} c={theme.primaryColor}>
               {vacancy.profession}
@@ -42,7 +45,7 @@ export default function VacancyCard({ vacancy }) {
             direction="row"
             justify="flex-start"
             align="center"
-            gap="xs"
+            gap={smallScreen ? 'xxs' : 'xs'}
             wrap="wrap"
           >
             <VacancyPayment
