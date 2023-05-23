efe0a8706a89ed8e81b652ef13418e0a64f60e84
IggyPope <altko4egar@gmail.com>
2023-05-23 15:21:52 +0300

    feat: add adaptive styling for VacancyHead

--- components/Vacancy/VacancyHead/VacancyHead.jsx
+++ components/Vacancy/VacancyHead/VacancyHead.jsx
@@ -1,13 +1,25 @@
-import { Flex, Paper, Stack, Text, Title } from '@mantine/core';
+import {
+  Flex,
+  Paper,
+  Stack,
+  Text,
+  Title,
+  useMantineTheme,
+} from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
+
 import LocationIcon from '@/components/Icons/LocationIcon';
 import VacancyPayment from '../common/VacancyPayment';
 import FavoriteButton from '@/components/Buttons/FavoriteButton';
 
 export default function VacancyHead({ vacancy }) {
+  const theme = useMantineTheme();
+  const smallScreen = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+
   return (
     <Paper
       radius="lg"
-      p="xl"
+      p={smallScreen ? 'sm' : 'xl'}
       w="100%"
       withBorder
       sx={(theme) => ({
@@ -16,7 +28,7 @@ export default function VacancyHead({ vacancy }) {
         },
       })}
     >
-      <Stack spacing="sm">
+      <Stack spacing={smallScreen ? 'xs' : 'sm'}>
         <Flex direction="row" justify="space-between" align="flex-start">
           <Title order={1}>{vacancy.profession}</Title>
           <FavoriteButton vacancy={vacancy} />
@@ -34,10 +46,10 @@ export default function VacancyHead({ vacancy }) {
             currency={vacancy.currency}
             titleOrder={3}
           />
-          <Text span inline c="gray.4" lh="21px" fz="md">
+          <Text span inline c="gray.4" lh="21px" fz={smallScreen ? 'sm' : 'md'}>
             •
           </Text>
-          <Text span inline fz="md">
+          <Text span inline fz={smallScreen ? 'sm' : 'md'}>
             {vacancy.type_of_work.title && vacancy.type_of_work.title}
           </Text>
         </Flex>
